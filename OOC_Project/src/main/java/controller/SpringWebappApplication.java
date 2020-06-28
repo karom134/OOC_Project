179f22e1d4e4b45d0e5b4e1937f40d5ae0431653
karom134 <karom.140598@gmail.com>
2020-06-28 16:15:36 +0100

    add spring basis

---
+++ OOC_Project/src/main/java/controller/SpringWebappApplication.java
@@ -0,0 +1,27 @@
+package controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SpringBootApplication
+@Configuration
+public class SpringWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebappApplication.class, args);
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp");//need change later
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+}
